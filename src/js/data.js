346d606b6899a7760dbf965c95021d0ca6cbb6c0
Hensgrej <hensgrej@gmail.com>
2018-08-09 09:16:29 -0500

    Agregando validación en inputs, el formulario no se manda si los datos estan mal escritos o incompletos

--- src/js/data.js
+++ src/js/data.js
@@ -9,19 +9,25 @@ var config = {
 };
 firebase.initializeApp(config);
 
-btnSend.addEventListener('click', (ev) => {
-  event.preventDefault(ev);
+btnSend.addEventListener('submit', (ev) => {
   let userNameValue = userName.value;
   let userLastNameValue = userLastName.value;
   let userEmailValue = userEmail.value;
   let userNumberValue = userNumber.value;
 
+  if (userEmail.validity.typeMismatch) {
+    userEmail.setCustomValidity('Ingresa un email valido');
+    event.preventDefault(ev);
+    window.history.back();
+  } else {
+    userEmail.setCustomValidity('');
+  }
   let db = firebase.firestore();
   let dbRef = db.collection('user').add({
     name: userNameValue,
     last_name: userLastNameValue,
     email: userEmailValue,
-    number: userNumberValue
+    number: userNumberValue,
   }).then(function(docRef) {
     console.log('Document written with ID: ', docRef.id);
   })
