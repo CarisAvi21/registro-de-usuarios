21ec9db9fe8aaab54b5c946142a86b769a482472
CarisAvi21 <shinnytinystar@hotmail.com>
2018-08-09 10:59:05 -0500

    bajando cambios

---
+++ src/js/data.js
@@ -0,0 +1,31 @@
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyAdd49IO1IpLA7KU2VcZithm89dPb_YxlU',
+  authDomain: 'registrousuarios-1c6e3.firebaseapp.com',
+  databaseURL: 'https://registrousuarios-1c6e3.firebaseio.com',
+  projectId: 'registrousuarios-1c6e3',
+  storageBucket: 'registrousuarios-1c6e3.appspot.com',
+  messagingSenderId: '615254550849'
+};
+firebase.initializeApp(config);
+
+btnSend.addEventListener('click', (ev) => {
+  event.preventDefault(ev);
+  let userNameValue = userName.value;
+  let userLastNameValue = userLastName.value;
+  let userEmailValue = userEmail.value;
+  let userNumberValue = userNumber.value;
+
+  let db = firebase.firestore();
+  let dbRef = db.collection('user').add({
+    name: userNameValue,
+    last_name: userLastNameValue,
+    email: userEmailValue,
+    number: userNumberValue
+  }).then(function(docRef) {
+    console.log('Document written with ID: ', docRef.id);
+  })
+    .catch(function(error) {
+      console.error('Error adding document: ', error);
+    });
+});
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+const userName = document.getElementById('user-name');
+const userLastName = document.getElementById('user-last-name');
+const userEmail = document.getElementById('user-email');
+const userNumber = document.getElementById('user-number');
+const btnSend = document.getElementById('btn-send');
