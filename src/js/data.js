69f0b4c85e89a0a7e11464ca4a06b6904a5b894e
Hensgrej <hensgrej@gmail.com>
2018-08-12 19:38:16 -0500

    Se acomoda funciones dentro de botón de envio. URL de la foto tomada se vincula con los datos ingresados, creando un perfil del usuario en la base de datos

--- src/js/data.js
+++ src/js/data.js
@@ -11,51 +11,63 @@ let config = {
 firebase.initializeApp(config);
 let db = firebase.firestore();
 let storage = firebase.storage();
-let blobURL = '';
-// WebCamera Functionality 
+let blobURL;
+
+// WebCamera Functionality
 let handleSuccess = function(stream) {
   // Attach the video stream to the video element and autoplay.
   player.srcObject = stream;
   videoTracks = stream.getVideoTracks();
-    
-  captureButton.addEventListener('click', function() {
+
+  captureButton.addEventListener('click', function(ev) {
+    event.preventDefault(ev);
     let context = snapshot.getContext('2d');
     // Draw the video frame to the canvas.
-    context.drawImage(player, 0, 0, snapshotCanvas.width, 
+    context.drawImage(player, 0, 0, snapshotCanvas.width,
       snapshotCanvas.height);
     videoTracks.forEach(function(track) {
       track.stop();
     });
 
-    snapshotCanvas.toBlob(function(blob) {
+    let blah = snapshotCanvas.toBlob(function(blob) {
       let newImg = document.createElement('img'),
         url = URL.createObjectURL(blob);
-      blobURL += url;
-      // console.log(url);
-      let ref = firebase.storage().ref('fotos/');
-      ref.put(blob).then(function(snapshot) {
-        // console.log('Uploaded a blob or file!');
+        let name = Math.random().toString(36).substring(7);
+        let ref = firebase.storage().ref().child('fotos/' + name);
+        ref.put(blob).then(function(snapshot) {
+        console.log('Uploaded a blob or file!');
+        ref.getDownloadURL().then(function(url) {
+        // This can be downloaded directly:
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+        xhr.onload = function(event) {
+          var blob = xhr.response;
+        };
+        xhr.open('GET', url);
+        xhr.send();
+        blobURL += url;
+      }).catch(function(error) {
+        console.log(error.message)
       });
     });
   });
-}; 
+});
+};
+
 navigator.mediaDevices.getUserMedia({video: true})
   .then(handleSuccess);
-      
+
 // Send Form
 btnSend.addEventListener('click', (ev) => {
   event.preventDefault(ev);
   let userNameValue = userName.value;
   let userEmailValue = userEmail.value;
-  let userNumberValue = userNumber.value;
-  
-
+  let photoURL = blobURL.substring(9);
   if (form.checkValidity() === true) {
     let dbRef = db.collection('user').add({
       name: userNameValue,
       email: userEmailValue,
-      number: userNumberValue,
-      blob: blobURL
+      blob: photoURL
     }).then(function(docRef) {
       // console.log('Document written with ID: ', docRef.id);
     })
@@ -65,10 +77,45 @@ btnSend.addEventListener('click', (ev) => {
   } else {
     form.reportValidity();
   }
+  // función para el envio de notificación por email
+  (function() {
+    emailjs.init('user_tQKEqfp1RDoxkEfhRcUTw');
+  })();
+
+  const vue = new Vue({
+    el: '#app',
+    data() {
+      return {
+        from_name: '',
+        from_email: '',
+      };
+    },
+    methods: {
+      enviar() {
+        let data = {
+          from_name: this.from_name,
+          from_email: this.from_email,
+        };
+
+        emailjs.send('gmail', 'notificaci_n_de_visita', data)
+          .then(function(response) {
+            if (response.text === 'OK') {
+              alert('El correo se ha enviado de forma exitosa');
+            }
+          // console.log('SUCCESS. status=%d, text=%s', response.status, response.text);
+          }, function(err) {
+            alert('Ocurrió un problema al enviar el correo');
+          // console.log('FAILED. error=', err);
+          });
+      }
+    }
+  });
+  // console.log(vue)
+  vue.enviar();
 });
 
 // Get Data from Database
-db.collection('user').get().then(function(querySnapshot) {
+/*db.collection('user').get().then(function(querySnapshot) {
   querySnapshot.forEach(function(doc) {
     // doc.data() is never undefined for query doc snapshots
     // console.log(doc.id, " => ", doc.data());
@@ -78,6 +125,7 @@ db.collection('user').get().then(function(querySnapshot) {
     // document.body.appendChild(newImg);
   });
 });
+*/
 
 // Obteniendo Info del JSON
 window.onload = () => {
@@ -86,42 +134,3 @@ window.onload = () => {
       listeners(data);
     });
 };
-
-
-// función para el envio de notificación por email
-
-(function() {
-  emailjs.init('user_tQKEqfp1RDoxkEfhRcUTw');
-})();
-
-const vue = new Vue({
-  el: '#app',
-  data() {
-    return {
-      from_name: '',
-      from_email: '',
-    };
-  },
-  methods: {
-    enviar() {
-      let data = {
-        from_name: this.from_name,
-        from_email: this.from_email,
-      };
-
-      emailjs.send('gmail', 'notificaci_n_de_visita', data)
-        .then(function(response) {
-          if (response.text === 'OK') {
-            alert('El correo se ha enviado de forma exitosa');
-          }
-          // console.log('SUCCESS. status=%d, text=%s', response.status, response.text);
-        }, function(err) {
-          alert('Ocurrió un problema al enviar el correo');
-          // console.log('FAILED. error=', err);
-        });
-    }
-  }
-});
-
-// console.log(vue)
-vue.enviar();
